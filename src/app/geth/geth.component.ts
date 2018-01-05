0565aaebd62b2da66bd82327d73e70f57ec03024
nagakedari <nagadurga.kedari@gmail.com>
2018-01-05 00:11:45 -0600

    how it works and geth tabs

---
+++ src/app/geth/geth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geth',
+  templateUrl: './geth.component.html',
+  styleUrls: ['./geth.component.css']
+})
+export class GethComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
